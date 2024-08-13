3290acf28249bfac86a0d425bf80796f98c7d356
Arslan203 <arslan.valeev.73@mail.ru>
2024-08-13 16:47:14 +0300

    add logging

--- WARP/train.py
+++ WARP/train.py
@@ -6,12 +6,25 @@ from data_utils import load_imdb_dataset, IMDbPrompts, collate_fn_WARP
 from functools import partial
 from WARP_impl import WARP_method
 from accelerate.utils import set_seed
+from transformers.utils import logging
+import transformers
 
 
 def train(args):
     np.random.seed(args['seed'])
     torch.manual_seed(args['seed'])
     set_seed(args['seed'])
+
+    verbosity_level = {
+        'FATAL': transformers.logging.FATAL,
+        'ERROR': transformers.logging.ERROR,
+        'WARNING': transformers.logging.WARN,
+        'WARN': transformers.logging.WARN,
+        'INFO': transformers.logging.INFO,
+        'DEBUG': transformers.logging.DEBUG,
+    }
+    logging.set_verbosity(verbosity_level[args['logging']])
+    logging.disable_progress_bar()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     dataset = load_imdb_dataset(args['dataset_name'])
